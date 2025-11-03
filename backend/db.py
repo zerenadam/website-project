dacb2e864d2db88dae6c0e575bb16f028f3fec19
zadam <zerenadam7@gmail.com>
2025-11-03 18:05:50 -0500

    database setup

--- backend/db.py
+++ backend/db.py
@@ -1,3 +1,15 @@
 import sqlite3
 
 
+class Database:
+
+
+    def __init__(self):
+
+        self.connection = sqlite3.connect("lost.db")
+        self.cursor = self.connection.cursor()
+        self.cursor.execute("CREATE TABLE IF NOT EXISTS items(id, desc, date, user)")
+
+    def add_item(self):
+        pass
+
